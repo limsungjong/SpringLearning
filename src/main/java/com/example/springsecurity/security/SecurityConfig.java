5507117e6e3ca0a7941319590461b4c588a3d172
limsungjong <sung8881@naver.com>
2023-07-31 17:44:05 +0900

    SecurityConfig 파일 하나로 병합

--- src/main/java/com/example/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.springsecurity.User.security;
+package com.example.springsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +8,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class UserSecurityConfig {
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain homeSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests((req) -> req
+                .antMatchers("/","/home").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
     @Bean
     public SecurityFilterChain userSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests((req) -> req
-                        .antMatchers("/","/home").permitAll()
-                        .anyRequest().authenticated()
+                        .antMatchers("/user").authenticated()
                 )
                 .formLogin((form) -> form
                         .loginPage("/login")
